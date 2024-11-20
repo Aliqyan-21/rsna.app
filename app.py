07841fb652eeb98721e1ffb888bd206c35cdc9b9
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-11-21 00:28:51 +0530

    fix: model load

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import pydicom
 import numpy as np
 from PIL import Image
+import torch.nn as nn
 import torch
 
 app = Flask(__name__)
@@ -12,8 +13,36 @@ UPLOAD_FOLDER = "static/uploads"
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
+# this class is architecture of the model -> dekh lo ye same hi architecture hona chahiye jaisa notebook me hai
+class SimpleCNN(nn.Module):
+    def __init__(self, coord_size=None):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.relu = nn.ReLU()
+        self.flattened_size = 128 * 39 * 39
+        self.coord_size = coord_size if coord_size is not None else 0
+        self.fc1_with_coords = nn.Linear(self.flattened_size + self.coord_size, 256)
+        self.fc1_without_coords = nn.Linear(self.flattened_size, 256)
+        self.fc2 = nn.Linear(256, 3)
+
+    def forward(self, x, coords=None):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
+        x = x.view(x.size(0), -1)  # Flatten
+        if coords is not None:
+            x = torch.cat((x, coords), dim=1)
+            x = self.relu(self.fc1_with_coords(x))
+        else:
+            x = self.relu(self.fc1_without_coords(x))
+        return self.fc2(x)
+
 model_path = "models/model.pth"
-model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
+model = SimpleCNN(coord_size=10) # it is 10 make it according to trained model
+model.load_state_dict(torch.load(model_path, map_location='cpu'))
 model.eval()
 
 @app.route('/')
